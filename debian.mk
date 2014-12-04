6ef41e84a126d785f36eb4ef3786f29165a5aa50
Aeluin Chen <aeluin.chen@moxa.com>
2014-12-04 11:43:30 +0800

    Add debian configuration files.

---
+++ debian.mk
@@ -0,0 +1,32 @@
+
+# Where to put executable commands/icons/conf on 'make install'?
+SANJI_VER   = 1.0
+RESOURCE    = network/route
+LIBDIR      = $(DESTDIR)/usr/lib/sanji-$(SANJI_VER)/$(RESOURCE)
+TMPDIR      = $(DESTDIR)/tmp
+
+FILES       = bundle.json route.py
+DIRS        = data ip
+
+
+all:
+	mkdir -p $(CURDIR)/packages
+	cp -a $(CURDIR)/requirements.txt $(CURDIR)/packages
+	pip install -r $(CURDIR)/packages/requirements.txt --download \
+		$(CURDIR)/packages || true
+
+clean:
+	# do nothing
+
+distclean: clean
+
+
+install: all
+	install -d $(LIBDIR)
+	install -d $(TMPDIR)
+	install $(FILES) $(LIBDIR)
+	cp -a $(DIRS) $(LIBDIR)
+	cp -a packages $(TMPDIR)
+
+uninstall:
+	-rm $(addprefix $(LIBDIR)/,$(FILES))
