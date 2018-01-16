a8474369933320d5f77acf639b0d23e08fbbac37
Aeluin Chen <aeluin.chen@moxa.com>
2018-01-16 10:44:05 +0800

    fix: internal use with actual interface

--- route/__init__.py
+++ route/__init__.py
@@ -140,11 +140,10 @@ class IPRoute(Model):
                 inet_ip = [inet["ip"]
                            for inet in iface_info["inet"]
                            if "" != inet["ip"]]
-                _iface = self._alias.get(iface, iface)
                 if len(inet_ip) and \
-                        (_iface in self._interfaces and
-                         self._interfaces[_iface]["status"] is True and
-                         self._interfaces[_iface]["wan"] is True):
+                        (iface in self._interfaces and
+                         self._interfaces[iface]["status"] is True and
+                         self._interfaces[iface]["wan"] is True):
                     data.append(iface)
         return data
 
@@ -163,7 +162,7 @@ class IPRoute(Model):
         self.try_update_default(self._routes)
         return self.model.db
 
-    def get_default(self):
+    def _get_default(self):
         """
         Retrieve current default gateway
 
@@ -181,23 +180,43 @@ class IPRoute(Model):
         default["wan"] = True
         default["status"] = True
         default["gateway"] = gw["default"]
-        if gw["dev"] in self._alias:
-            default["interface"] = self._alias[gw["dev"]]
-            default["actualIface"] = gw["dev"]
-        else:
-            default["interface"] = gw["dev"]
+        default["interface"] = gw["dev"]
         return default
 
-    def _update_default(self, iface=None, gateway=None):
+    def get_default(self):
+        default = self._get_default()
+        _iface = default["interface"]
+        if _iface in self._alias:
+            default["interface"] = self._alias[_iface]
+            default["actualIface"] = _iface
+        return default
+
+    def _update_default(self, default):
         """
         Update default gateway. If updated failed, should recover to previous
         one.
 
         Args:
-            iface: interface
-            gateway: IP address of default gateway
+            default: dict format, require at least one of "interface" and
+                     "gateway"
+                example:
+                {
+                  "interface": "ppp0",
+                  "alias": "wwan0",
+                  "gateway": "192.168.7.254",
+                  "status": true,
+                  "wan": true
+                }
         """
         ip.route.delete("default")
+
+        iface = None
+        gateway = None
+        if "interface" in default and default["interface"]:
+            iface = default["interface"]
+        if "gateway" in default and default["gateway"]:
+            gateway = default["gateway"]
+
         if not iface and not gateway:
             raise IPRouteError("Invalid default route.")
 
@@ -220,33 +239,6 @@ class IPRoute(Model):
         else:
             raise IPRouteError("Invalid default route.")
 
-    def update_default(self, default):
-        """
-        Update default gateway. If updated failed, should recover to previous
-        one.
-
-        Args:
-            default: dict format, require at least one of "interface" and
-                     "gateway"
-                example:
-                {
-                  "interface": "wwan0",
-                  "actualIface": "ppp0",
-                  "gateway": "192.168.7.254",
-                  "status": true,
-                  "wan": true
-                }
-        """
-        iface = None
-        gateway = None
-        if "actualIface" in default and default["actualIface"]:
-            iface = default["actualIface"]
-        elif "interface" in default and default["interface"]:
-            iface = default["interface"]
-        if "gateway" in default and default["gateway"]:
-            gateway = default["gateway"]
-        return self._update_default(iface, gateway)
-
     def _try_update_default(self, routes):
         """
         Try to update the default gateway.
@@ -259,25 +251,25 @@ class IPRoute(Model):
         ifaces = self.list_interfaces()
         if not ifaces:
             # FIXME: keep or clean?
-            # self.update_default({})
+            # self._update_default({})
             raise IPRouteError("Interfaces should be UP.")
 
         default = {}
         for iface in routes:
             if iface in ifaces:
-                default["interface"] = self._alias.get(iface, iface)
+                default["interface"] = iface
                 break
         else:
-            self.update_default({})
+            self._update_default({})
             return
 
         # find gateway by interface
         default.update(self._interfaces[default["interface"]])
 
-        current = self.get_default()
+        current = self._get_default()
         if current.get("interface", "") != default.get("interface", "") or \
                 current.get("gateway", "") != default.get("gateway", ""):
-            self.update_default(default)
+            self._update_default(default)
 
     def try_update_default(self, routes):
         with _update_default_lock:
@@ -286,12 +278,13 @@ class IPRoute(Model):
             except IPRouteError as e:
                 _logger.debug(e)
 
-    def set_default(self, default):
+    def _set_default(self, default):
         """
         Update default gateway by given info.
         """
+        # TODO: unused, remove or keep?
         try:
-            self.update_default(default)
+            self._update_default(default)
         except Exception as e:
             # try database if failed
             try:
@@ -314,21 +307,29 @@ class IPRoute(Model):
         Args:
             interface: dict format with interface "name" and/or "gateway".
         """
-        if "status" not in iface:
-            iface["status"] = True
-        if "wan" not in iface:
-            iface["wan"] = True
+        iface["status"] = iface.get("status", True)
+        iface["wan"] = iface.get("wan", True)
+
+        if "actualIface" in iface:
+            name = iface.get("actualIface")
+            alias = iface.get("name", name)
+        else:
+            name = iface.get("name")
+            alias = None
+        if not name:
+            error = "Interface name not given."
+            _logger.error(error)
+            raise IPRouteError(error)
 
         # update the router information
-        name = iface["name"]
         if name not in self._interfaces:
             self._interfaces[name] = {}
         self._interfaces[name]["status"] = iface["status"]
         self._interfaces[name]["wan"] = iface["wan"]
         if "gateway" in iface:
             self._interfaces[name]["gateway"] = iface["gateway"]
-        if "actualIface" in iface:
-            self._interfaces[name]["actualIface"] = iface["actualIface"]
+        if alias:
+            self._interfaces[name]["alias"] = alias
 
         # update interface list
         self._routes = self._get_priority_list()
@@ -337,7 +338,19 @@ class IPRoute(Model):
         self.try_update_default(self._routes)
 
     def get_iface_db(self):
-        return self._interfaces
+        db = {}
+        for iface in self._interfaces.keys():
+            item = self._interfaces[iface]
+            if "alias" not in item:
+                db[iface] = item.copy()
+            else:
+                db[item["alias"]] = {
+                    "status": item.get("status", True),
+                    "wan": item.get("wan", True),
+                    "gateway": item.get("gateway", ""),
+                    "actualIface": iface
+                }
+        return db
 
 
 if __name__ == "__main__":
